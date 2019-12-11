43d48ffb7444588c85c8bfc89dd62134422fe5e9
Orlando Carnate <orlando.carnate@gmail.com>
2019-12-11 15:13:57 -0600

    Lesson 13: Oculus Rift setup

---
+++ Assets/Scripts/OculusInputFocus.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OculusInputFocus : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        OVRManager.InputFocusAcquired += OnInputFocusAcquired;
+        OVRManager.InputFocusLost += OnInputFocusLost;
+    }
+
+    private void OnInputFocusAcquired()
+    {
+        Debug.Log("Input Focus Acquired, resume the game");
+        Time.timeScale = 1;
+    }
+
+    private void OnInputFocusLost()
+    {
+        Debug.Log("Input Focus Lost, pause");
+        Time.timeScale = 0;
+    }
+}
